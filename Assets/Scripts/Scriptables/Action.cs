be261313930cd9e7eb4d13aa0ddc298db6de5c34
Derek Nguyen <nguye626@msu.edu>
2019-10-20 09:01:03 -0400

    Created Testing Room

---
+++ Assets/Scripts/Scriptables/Action.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace TG.Scriptable
+{
+    [CreateAssetMenu(menuName = "Action")]
+    public class Action : MonoBehaviour
+    {
+        public Object action_obj;
+    }
+
+    public enum ActionType
+    {
+        attack, block, parry
+    }
+}
