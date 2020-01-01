5f8680fe489f6abafd4075a65135d0d1e3484cab
Derek Nguyen <nguye626@msu.edu>
2020-01-01 06:00:23 -0500

    Created Room Transitions and Fixed Dodge bugs

---
+++ Assets/LevelMover.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelMover : MonoBehaviour
+{
+    [SerializeField]
+    private int currentLevel;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Player")
+        {
+            SceneManager.LoadScene(currentLevel + 1);
+        }
+    }
+}
---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelManager : MonoBehaviour
+{
+    private bool m_Completed = false;
+    private GameObject m_Gate;
+    [SerializeField]
+    private List<Stats> enemies;
+
+    private void Start()
+    {
+        m_Gate = GameObject.Find("Gate");
+        m_Gate.SetActive(false);
+    }
+
+    private void Update()
+    {
+        foreach(Stats thing in enemies)
+        {
+            if (thing.IsDead())
+                enemies.Remove(thing);
+        }
+        if(enemies.Count == 0)
+        {
+            m_Gate.SetActive(true);
+        }
+    }
+
+
+
+
+}
