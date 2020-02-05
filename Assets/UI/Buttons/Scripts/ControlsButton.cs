76c61a4d78b8501c5a6b3bd39f26a63a88516858
Derek Nguyen <nguye626@msu.edu>
2020-02-05 03:37:55 -0500

    Added a controls screen

---
+++ Assets/UI/Buttons/Scripts/ControlsButton.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControlsButton : MenuButton
+{
+    public GameObject window;
+
+    public override void ButtonEffects()
+    {
+        window.SetActive(true);
+    }
+}
